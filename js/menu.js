21180bb908fdd63f5a2ab9e89c4b56d47478a6e0
Jaja <a926a926@gmail.com>
2016-05-01 01:10:31 +0800

    * my web process 1. navbar area adds slide effect.(next step: modify the effect.) 2. click navbar item will ahead to the right position. ( .animate() ) 3. back button: scroll to the top.( .scrollTop() ) 4. put the video under the purple background and re-size it. 5. video effect: when clicking the play button, the video will be played and hide the purple background, play icon.                  while the video is over, purple background and play icon show again. ----------- jQuery used: .on()  .scrollTop()  .hide() .show()  .slick() .click() .animate()

---
+++ js/menu.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="jquery-1.12.3.js" /> 
+$(function () {  //1.點下按鈕之後會以 animate動態效果讓捲軸捲到網頁最頂端
+    $("#gotop").click(function () {
+        jQuery("html,body").animate({ scrollTop:0 }, 800);
+    });
+    //2.$(this).scrollTop()就是用來取得目前的位置距離網頁頂端有多遠，若大於 300px 就會將按鈕顯示出來，小於就會隱藏。
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('#gotop').fadeIn();
+        } else {
+            $('#gotop').stop().fadeOut();
+        }
+    });
+
+    //1.先讓按鈕有按下然後執行內容的效果
+    $('#menu-about').click(function () {
+        //2.利用animate移到到指定的區塊
+        $('html,body').animate({ scrollTop: $('.vedioplay').offset().top }, 800);
+    });
+
+    $('#menu-teams').click(function () {
+        $('html,body').animate({ scrollTop: $('.team').offset().top }, 800);
+    })
+
+    $('#menu-works').click(function () {
+        $('html,body').animate({ scrollTop: $('.work').offset().top }, 800);
+    })
+
+    $('#menu-peoplesay').click(function () {
+        $('html,body').animate({ scrollTop: $('.testimonials').offset().top }, 800);
+    })
+
+    $('#menu-contact').click(function () {
+        $('html,body').animate({ scrollTop: $('.contact').offset().top }, 800);
+    })
+
+    //start use my carousel
+    $('.my-carousel').slick({
+        autoplay: true,
+        arrows: false,
+        dots:true
+    });
+   
+    
+    $('.playicon').click(function () {   //1.按下按鈕-影片播放
+        $('.my-video').get(0).play();
+        $('.v-top').hide();              //2.1隱藏紫色布幕
+        $('.vedio-zone').hide();        //2.2隱藏播放按鈕
+        $('.my-video').on('ended', function () {       //3.當影片播放完畢,回到原始狀態
+            $('.v-top').show();
+            $('.vedio-zone').show();
+        })
+    })
+    
+    
+
+    
+});
