e233be2dfb9ef7f633d760fdbdead2270c37f22a
Jaja <a926a926@gmail.com>
2016-05-01 17:35:14 +0800

    * my web process 1. testimonials: 圖片輪播效果     1.1 動畫效果要設在slick-center class裡面     1.2 slick-track 要變寬，不然圖示對被切掉(內容要調margin)     1.3 transform 搭配 transition 設置動畫效果 2. add story effect: slideToggle() 收合效果  css: dispaly:none (一開始就把原本的div隱藏起來)

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,11 @@
 ﻿/// <reference path="jquery-1.12.3.js" /> 
-$(function () {  //1.點下按鈕之後會以 animate動態效果讓捲軸捲到網頁最頂端
+
+$(function () {  
+
+
+    /////////navbar effect
     $("#gotop").click(function () {
-        jQuery("html,body").animate({ scrollTop:0 }, 800);
+        jQuery("html,body").animate({ scrollTop: 0 }, 800); //1.點下按鈕之後會以 animate動態效果讓捲軸捲到網頁最頂端
     });
     //2.$(this).scrollTop()就是用來取得目前的位置距離網頁頂端有多遠，若大於 300px 就會將按鈕顯示出來，小於就會隱藏。
     $(window).scroll(function () {
@@ -41,7 +45,17 @@ $(function () {  //1.點下按鈕之後會以 animate動態效果讓捲軸捲到
         dots:true
     });
    
-    
+
+    ///////// story effect
+
+    $(".learnmore2").click(function () {
+        $(".morestory").slideToggle(1000);
+    });
+
+
+
+
+    ///////// Videi effect
     $('.playicon').click(function () {   //1.按下按鈕-影片播放
         $('.my-video').get(0).play();
         $('.v-top').hide();              //2.1隱藏紫色布幕
@@ -52,7 +66,14 @@ $(function () {  //1.點下按鈕之後會以 animate動態效果讓捲軸捲到
         })
     })
     
-    
+    ///////////testimonials effect
 
+    $(document).ready(function(){
+        $('.circle-pic').slick({
+                centerMode: true,
+                centerPadding: '60px',
+                slidesToShow: 3
+            });
+        });
     
 });
