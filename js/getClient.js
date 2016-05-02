14af79f51add9b3f7255f7de41cb47e1eb923371
Jaja <a926a926@gmail.com>
2016-05-03 00:16:40 +0800

    * my web process(90%): 1. fixed the problem of navbar area(the part of missing text) 2. modify the testimonials area
    ----------
    
    json practice:
    1.練習jQuery.get和jQuery.post
    2.jQuery迴圈$.each()，陣列都和迴圈使用
    3.google page speed: 檢視網頁速度，會提供可改進地方
    4.分清楚陣列array和物件object的使用

--- js/getClient.js
+++ js/getClient.js
@@ -14,16 +14,24 @@ $(function () {
         .done(function (myemail) {
             //console.log(myemail);  //可用console去看回傳的東西是什麼：陣列
 
-            if (myemail.length > 0) {   // 判斷陣列的長度若>0：是有相對應的
-               // console.log(myemail[0].Name)
-                $('.result').eq(0).html("Name:" + myemail[0].Name + "<br/>" +   //jQuery：想要顯示的東西
-                                        "ID:" + myemail[0].ID + "<br/>" +
-                                        "Subject:" + myemail[0].Subject + "<br/>" +
-                                        "Email:" + myemail[0].Email + "<br/>" +
-                                        "Message:" + myemail[0].Message);
-
-
-                //document.write("Name:" + myemail[0].Name + "<br/>" +      
+            if (myemail.length > 0) {   // 判斷陣列的長度若>0：有查尋到內容
+                // console.log(myemail[0].Name)
+                var mycontent = "";
+                $.each(myemail, function (index, value) {
+                   console.log(value);
+                   mycontent = mycontent +"ID:"+ value.ID+"ID:"+value.Name+"ID:"+value.Email+"ID:"+value.Subject+"ID:"+value.Message+"<br/>";
+                })
+                //for (var i = 0; i < myemail.length; i++) {
+                //    mycontent = mycontent + "Name:" + myemail[i].Name + "<br/>" +   //使用jQuery顯示回傳的內容
+                //                        "ID:" + myemail[i].ID + "<br/>" +
+                //                        "Subject:" + myemail[i].Subject + "<br/>" +
+                //                        "Email:" + myemail[i].Email + "<br/>" +
+                //                        "Message:" + myemail[i].Message;
+                //}
+                $('.result').eq(0).html(mycontent);
+                                        
+                
+                //document.write("Name:" + myemail[0].Name + "<br/>" +      //使用document.write時，會將內容顯示在整個網頁，並取代原本的內容
                 //               "ID:" + myemail[0].ID + "<br/>" +
                 //               "Subject:" + myemail[0].Subject + "<br/>" +
                 //               "Email:" + myemail[0].Email + "<br/>" +
@@ -34,20 +42,29 @@ $(function () {
             }
             //result.innereHTML = myemail;
             //"aaa@abc.asdf".innereHTML = myemail;
-
-
-        })
-    
-    
-    
-    
-    
+        })      
     })
 
 
 
+    //////////////////////  DOG
+    $('.dog').click(function () {
+        $.get("https://data.cityofchicago.org/api/views/qjr3-bm53/rows.json?accessType=DOWNLOAD")
+        .done(function (dogdata) {
+           console.log(dogdata);
+           var alldata = "";
+           $.each(dogdata.data, function (index, value) {
+                console.log(value[1],value[9],value[10]);
+                alldata = alldata + index + ":" + "     No.1:  " + value[1] + "    No.2:" + value[9] + "   No.3:" + value[10] + "<br/>";
+            //   // console.log(alldata)
 
+            //})
+            $('.dog-result').eq(0).html(alldata);
+        })
+        });
 
+        
 
+    })
 
 })
